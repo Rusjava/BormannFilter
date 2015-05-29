f58620f1c07e7ec69e3260de63e1117d54a4df66
Samsung <Samsung@127.0.0.1>
2015-05-29 08:35:14 +0400

    Expanded XrayCrystall class

--- src/borrmannfilter/XrayCrystall.java
+++ src/borrmannfilter/XrayCrystall.java
@@ -24,42 +24,112 @@ import org.apache.commons.math3.complex.*;
  * @version 0.1
  */
 public class XrayCrystall {
+
     private double d, den, theta;
     private Complex f;
-    
-    public XrayCrystall () {
+
+    public XrayCrystall() {
         this.f = new Complex(0, 0);
     }
-    
+
     /**
      * Returning real part of the scattering factor
+     *
      * @return
      */
-    public double getF1 () {
+    public double getF1() {
         return f.getReal();
     }
-    
+
     /**
      * Setting real part of the scattering factor
+     *
      * @param f1
      */
-    public void setF1 (double f1) {
+    public void setF1(double f1) {
         f = new Complex(f1, f.getImaginary());
     }
-    
+
     /**
      * Returning imaginary part of the scattering factor
+     *
      * @return
      */
-    public double getF2 () {
+    public double getF2() {
         return f.getImaginary();
     }
-    
+
     /**
      * Setting imaginary part of the scattering factor
+     *
      * @param f2
      */
-    public void setF2 (double f2) {
+    public void setF2(double f2) {
         f = new Complex(f.getReal(), f2);
+    }
+
+    /**
+     * Returning inter-plane distance of the scattering factor
+     *
+     * @return
+     */
+    public double getD() {
+        return d;
+    }
+
+    /**
+     * Setting inter-plane distance of the scattering factor
+     *
+     * @param d
+     */
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    /**
+     * Returning density of the scattering factor
+     *
+     * @return
+     */
+    public double getDen() {
+        return den;
+    }
+
+    /**
+     * Setting density of the scattering factor
+     *
+     * @param den
+     */
+    public void setDen(double den) {
+        this.den = den;
+    }
+
+    /**
+     * Returning cutting angle of the scattering factor
+     *
+     * @return
+     */
+    public double getTheta() {
+        return theta;
+    }
+
+    /**
+     * Setting cutting angle of the scattering factor
+     *
+     * @param theta
+     */
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    /**
+     * Returning diffraction angle
+     *
+     * @param phi incidence angle
+     * @param wl wavelength
+     * @return
+     */
+    public double getDiffAngle(double phi, double wl) {
+        return Math.asin(Math.sin(phi) + wl / d * Math.sin(theta));
     }
 }
